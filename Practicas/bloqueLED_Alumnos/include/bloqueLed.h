26fb44c054461099f1ba6809902c8aa36f25fcb2
rafa <rafaellg8@correo.ugr.es>
2016-03-16 00:24:18 +0100

    all

---
+++ Practicas/bloqueLED_Alumnos/include/bloqueLed.h
@@ -0,0 +1,124 @@
+/**
+@file
+@author Rafael Lachica Garrido
+@date 16 de febrero de 2015, 19:33
+@brief Funciones de manejo de bloqueLeds
+
+*/
+
+#ifndef _BLOQUELED_H_
+#define	_BLOQUELED_H_
+
+typedef unsigned char bloqueLed; ///< Un @c bloqueLed contiene el estado de 8 LEDs
+
+/**
+@brief enciende el LED @p pos del @c bloqueLed @p b
+@param b 	el @c bloqueLed cuyo LED se quiere activar
+@param pos 	el LED dentro de @p b que se quiere activar (0 m�s a la derecha)
+*/
+void on(bloqueLed &b, int pos);
+
+/**
+@brief apaga el LED @p pos del  @c bloqueLed @p b
+@param b 	el @c bloqueLed cuyo LED se quiere desactivar
+@param pos 	el LED dentro de @p b que se quiere desactivar (0 m�s a la derecha)
+*/
+void off(bloqueLed &b, int pos){
+}
+
+/**
+@brief devuelve el estado del LED (encendido = true, apagado = false) en la posici�n @p pos
+@param b 	el @c bloqueLed que se quiere consultar
+@param pos 	el LED dentro de @ b que se quiere consultar (0 m�s a la derecha)
+@retval true 	si el LED en la posici�n @p pos est� encendido
+@retval false	si el LED en la posici�n @p pos est� apagado
+*/
+bool get(bloqueLed b, int pos);
+
+/**
+@brief Muestra por la salida est�ndar una secuencia de 0s y 1s correspondiente  al estado de cada LED
+@param b el @c bloqueLed que se quiere imprimir
+
+Muestra por la salida est�ndar una secuencia de 0s y 1s correspondiente  al estado de cada LED del bloqueLed donde un cero representa que un LED est� apagado y un uno que el LED est� encendido. Se implementa utilizando la funci�n "get".
+
+Por ejemplo, si en el @c bloqueLed @c b est�n encendidos los LEDs en posiciones 1 y 3 (0 m�s a la derecha), @c print(b); mostrar� @c 00001010
+
+*/
+void print(bloqueLed b);
+
+/**
+@brief enciende todos los LEDs
+@param b el @c bloqueLed que se quiere encender completamente.
+*/
+void encender(bloqueLed &b){
+  b = 0b11111111;
+}
+/**
+@brief apaga todos los LEDs
+@param b el @c bloqueLed que se quiere apagar completamente.
+*/
+void apagar(bloqueLed &b){
+  b = 0b00000000;
+}
+
+/**
+@brief enciende los LEDs seg�n la configuraci�n de @p v
+@param b el @c bloqueLed sobre el que se quiere actuar
+@param v vector de 8 elementos que contiene la secuencia de LEDS que se quieren asignar
+
+Asigna a @p b la configuraci�n de LEDs contenida en @p v. @p v es un vector de 8 booleanos donde @c true significa encendido y @c false significa apagado.
+*/
+void asignar(bloqueLed &b, const bool v[]);
+
+/**
+@brief recupera la configuraci�n de todos los LED
+@param b el @c bloqueLed que se quiere consultar
+@param v vector de 8 elementos que contendr� el estado de cada uno de los LEDs de @ b
+
+Vuelca en @p v la configuraci�n de LEDs contenida en @p b. @c true significa encendido y @c false significa apagado. El tama�o de @p v debe ser 8.
+*/
+void volcar(bloqueLed b, bool v[]);
+
+/**
+@brief devuelve las posiciones de los LEDs encendidos
+@param b el @c bloqueLed que se quiere consultar
+@param posic vector de enteros (valores entre 0 a 7) que indican la posici�n de los LEDs de @p b que est�n encendidos
+@param cuantos n�mero de LEDs encendidos en @p b (n�mero de elementos usados en el vector @p posic)
+*/
+void encendidos(bloqueLed b, int posic[], int &cuantos);
+
+/**
+@brief enmascara a binario una posicion
+@param numero de posicion
+*/
+bloqueLed enmascara(int numero){
+  bloqueLed binario;
+  switch (numero) {
+    case 0:
+      binario = 0b00000000;
+    break;
+    case 1:
+      binario = 0b00000010;
+    break;
+    case 2:
+      binario = 0b00000100;
+    break;
+    case 3:
+      binario = 0b00001000;
+    break;
+    case 4:
+      binario = 0b00010000;
+    break;
+    case 5:
+      binario = 0b00100000;
+    break;
+    case 6:
+      binario = 0b01000000;
+    break;
+    case 7:
+      binario = 0b10000000;
+    break;
+  }
+  return binario;
+}
+#endif
